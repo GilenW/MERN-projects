4c32e86bc5cbab3f17e2dc639976528ee9664996
G-macbook <gwuhou@ucsd.edu>
2024-10-11 20:45:10 -0700

    delete experience

--- platform/backend/routes/api/profile.js
+++ platform/backend/routes/api/profile.js
@@ -228,6 +228,30 @@ router.put(
         }
     }
 );
+
+//route DELETE api/profile/experience/:exp_id
+//@desc Delete profile experience
+//@access private
+
+router.delete('/experience/:exp_id', auth, async (req, res) =>
+{
+    try {
+        const profile = await Profile.findOne({
+            user: req.user.id,
+        });
+
+        const removeIndex = profile.experiences.map(item => item.id).indexOf(req.params.exp_id);
+
+        profile.experiences.splice(removeIndex, 1);
+        await profile.save()
+		res.json(profile);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server error deleting profile')
+    }
+})
+
+
 module.exports = router;
 
 
